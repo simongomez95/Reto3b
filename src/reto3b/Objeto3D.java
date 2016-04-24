cf63839bb3c89888829c0b2ca3e382b79ca75af1
simon gomez <nekothecat@MacBook-de-simon.local>
2016-04-24 12:48:36 -0500

    Se detecta el zMenor de cada objeto para luego contrastarlo con los demas y determinar orden de renderizado

--- src/reto3b/Objeto3D.java
+++ src/reto3b/Objeto3D.java
@@ -10,9 +10,19 @@ public class Objeto3D {
     private List<int[]> listaAristas = new LinkedList<int[]>();
     private List<Punto3Dh> listaPuntos = new LinkedList<Punto3Dh>();
 
+    private double zMenor;
+
     public Objeto3D(List<int[]> aristas, List <Punto3Dh> puntos) {
         listaAristas = aristas;
         listaPuntos = puntos;
+
+        //Determinamos el Z menor del Objeto
+        zMenor = puntos.get(0).getZ();
+        for(Punto3Dh punto : puntos) {
+            if (punto.getZ() < zMenor) {
+                zMenor = punto.getZ();
+            }
+        }
     }
 
     public List<Punto3Dh> getPuntos() {
@@ -21,5 +31,9 @@ public class Objeto3D {
 
     public List<int[]> getAristas() {
         return listaAristas;
+    }
+
+    public double getzMenor() {
+        return zMenor;
     }
 }
